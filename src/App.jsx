90209596780086d8b2c07b9aa692a57a9049de1f
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-08-28 17:12:27 +0800

    create: Banner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/Navbar/Navbar'
+import { Banner } from './components/Banner/Banner'
 
 function App() {
 
   return (
     <div>
       <NavBar />
-      <h1>Banner</h1>
+      <Banner />
       <h1>Skills</h1>
       <h1>Projects</h1>
       <h1>Contact</h1>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,6 @@ import {
 import './Navbar.css';
 
 export const NavBar = () => {
-
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
