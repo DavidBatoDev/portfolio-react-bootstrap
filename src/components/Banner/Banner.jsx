e11c10d755927cac6a926f1879df8faefb1a5900
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-08-29 23:37:40 +0800

    chore: Update navbar and skills components

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from '../../assets/img/header-img.svg'
+import headerImg from '../../assets/img/header-img.jpg'
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -67,7 +67,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 // <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                <div className={isVisible ? "" : ""}>
+                <div className={isVisible ? "flex" : "flex"}>
                   <img src={headerImg} alt="Header Img"/>
                 </div>}
             </TrackVisibility>
